49f3d53248758badd830a7d8aa4933eed205f9ac
NamSangKyu <nam2626@naver.com>
2026-05-29 10:25:50 +0900

    Update step03 bean lifecycle example

--- step03-ioc-bean/src/main/java/com/spring/life_cycle/LifecycleMainApp.java
+++ step03-ioc-bean/src/main/java/com/spring/life_cycle/LifecycleMainApp.java
@@ -1,15 +1,24 @@
 package com.spring.life_cycle;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class LifecycleMainApp {
 
 	public static void main(String[] args) {
         System.out.println("▶ ApplicationContext 생성 시작\n");
-
-		//DatabaseConnector 생성 
+        ApplicationContext context = 
+        		new ClassPathXmlApplicationContext("applicationContext-lifecyle.xml");
         
+		//DatabaseConnector 생성 
+        DatabaseConnector connector1 = context.getBean(DatabaseConnector.class);
+        connector1.query("select * from member");
+        DatabaseConnector connector2 = context.getBean(DatabaseConnector.class);
 		
         System.out.println("\n▶ 컨테이너 종료(close()) 시작\n");
-        
+        // 소멸할때 호출되는 메서드는 scope가 singleton 일때만 호출됨
+        // prototype은 호출이 안됨 - IoC가 생성후에 관리를 안함
+        ((ClassPathXmlApplicationContext) context).close(); 
 	}
 
 }
