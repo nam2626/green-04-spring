399f32a6a134198069a74ce551ac9711505fcf79
NamSangKyu <nam2626@naver.com>
2026-05-29 10:14:20 +0900

    Add step03 IoC bean lifecycle example

---
+++ step03-ioc-bean/src/main/java/com/spring/life_cycle/LifecycleMainApp.java
@@ -0,0 +1,15 @@
+package com.spring.life_cycle;
+
+public class LifecycleMainApp {
+
+	public static void main(String[] args) {
+        System.out.println("▶ ApplicationContext 생성 시작\n");
+
+		//DatabaseConnector 생성 
+        
+		
+        System.out.println("\n▶ 컨테이너 종료(close()) 시작\n");
+        
+	}
+
+}
