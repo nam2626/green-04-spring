82a2b6bb74f80d19c01372a9c82c7032ddb3a82e
NamSangKyu <nam2626@naver.com>
2026-06-01 15:00:39 +0900

    Add greet functionality to HelloController and create greet.jsp view

--- step06-mvc-basic/src/main/java/com/spring/controller/HelloController.java
+++ step06-mvc-basic/src/main/java/com/spring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/hello")
@@ -13,4 +14,25 @@ public class HelloController {
 		model.addAttribute("greeting", "안녕하세요. hello 컨트롤러 입니다.");
 		return "hello";
 	}
+
+	/*
+	  '/hello/greet'
+	*/
+	@GetMapping("/greet")
+	public String greet(@RequestParam(name = "name", defaultValue = "손님") String name,
+			Model model) {
+		model.addAttribute("name", name);
+		model.addAttribute("message", name + "님, 반갑습니다.");
+		return "greet";
+	}
+	
+	
 }
+
+
+
+
+
+
+
+
--- step06-mvc-basic/src/main/webapp/WEB-INF/views/home.jsp
+++ step06-mvc-basic/src/main/webapp/WEB-INF/views/home.jsp
@@ -11,6 +11,15 @@
 	<p>서버시간 : ${serverTime }</p>
 	<ul>
 		<li><a href="${pageContext.request.contextPath }/hello">인사페이지</a></li>
+		<li>
+			<a href="${pageContext.request.contextPath }/hello/greet?name=홍길동">
+				홍길동으로 인사
+			</a>
+		</li>
 	</ul>
 </body>
 </html>
+
+
+
+
