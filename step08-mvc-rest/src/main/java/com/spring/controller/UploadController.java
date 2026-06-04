6f8399829c10295795565e09d2624af221b52e53
NamSangKyu <nam2626@naver.com>
2026-06-04 14:38:56 +0900

    Add process method to UploadController for handling file uploads

--- step08-mvc-rest/src/main/java/com/spring/controller/UploadController.java
+++ step08-mvc-rest/src/main/java/com/spring/controller/UploadController.java
@@ -1,8 +1,14 @@
 package com.spring.controller;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/upload")
@@ -12,5 +18,30 @@ public class UploadController {
 	public String form() {
 		return "upload/form";
 	}
+	
+	@PostMapping("/process")
+	public ModelAndView process(MultipartFile file,ModelAndView view) throws IllegalStateException, IOException {
+		if(file.isEmpty()) {
+			view.addObject("message","파일을 선택하세요");
+		}else {
+			// 업로드한 파일명
+			view.addObject("fileName",file.getOriginalFilename());
+			view.addObject("fileSize",file.getSize());
+			view.addObject("contentType",file.getContentType());
+			view.addObject("message","파일 업로드 성공");
+			//실제 파일 저장하는 부분
+			File root = new File("c:\\fileupload");
+			File fpath = new File(root.getAbsoluteFile() + "\\" + file.getOriginalFilename());
+			file.transferTo(fpath);
+		}
+		view.setViewName("upload/result");
+		
+		return view;
+	}
 
 }
+
+
+
+
+
