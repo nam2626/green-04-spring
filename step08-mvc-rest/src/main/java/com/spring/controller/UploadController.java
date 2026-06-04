24743d2d58f3f1f75783a1eae71fe599b3dc5a5d
NamSangKyu <nam2626@naver.com>
2026-06-04 15:02:34 +0900

    Update UploadController to use @RequestParam for file uploads and modify form to allow multiple file selections

--- step08-mvc-rest/src/main/java/com/spring/controller/UploadController.java
+++ step08-mvc-rest/src/main/java/com/spring/controller/UploadController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -20,7 +21,7 @@ public class UploadController {
 	}
 	
 	@PostMapping("/process")
-	public ModelAndView process(MultipartFile file,ModelAndView view) throws IllegalStateException, IOException {
+	public ModelAndView process(@RequestParam("file") MultipartFile file,ModelAndView view) throws IllegalStateException, IOException {
 		if(file.isEmpty()) {
 			view.addObject("message","파일을 선택하세요");
 		}else {
--- step08-mvc-rest/src/main/webapp/WEB-INF/views/upload/form.jsp
+++ step08-mvc-rest/src/main/webapp/WEB-INF/views/upload/form.jsp
@@ -4,8 +4,10 @@
 <head><meta charset="UTF-8"><title>파일 업로드</title></head>
 <body>
   <h2>파일 업로드</h2>
-  <form method="post" action="process" enctype="multipart/form-data">
-    <input type="file" name="file"/>
+  <form method="post" action="${pageContext.request.contextPath }/upload/process" enctype="multipart/form-data">
+    <input type="file" name="file"/><br>
+    <input type="file" name="file"/><br>
+    <input type="file" name="file"/><br>
     <button type="submit">업로드</button>
   </form>
 </body>
