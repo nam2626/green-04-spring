43dbe907517b0e91eb45f5d37aeb61cfb5bd9bae
NamSangKyu <nam2626@naver.com>
2026-06-05 11:15:40 +0900

    Add MenuController and Thymeleaf template for menu list; implement menu listing functionality

--- step09-spring-thymleaf/src/main/java/com/spring/controller/MenuController.java
+++ step09-spring-thymleaf/src/main/java/com/spring/controller/MenuController.java
@@ -3,7 +3,9 @@ package com.spring.controller;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.spring.dto.MenuDTO;
 
@@ -16,4 +18,22 @@ public class MenuController {
 			new MenuDTO(2L, "치킨버거", "버거", 9000), 
 			new MenuDTO(3L, "새우버거", "버거", 8000),
 			new MenuDTO(4L, "아이스아메리카노", "음료", 3500));
+	
+	@GetMapping
+	public ModelAndView list(ModelAndView view) {
+		view.addObject("menus", menuList);
+		view.setViewName("menu/list");
+		return view;
+		
+	}
+	
+	
 }
+
+
+
+
+
+
+
+
