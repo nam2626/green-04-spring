bbd892842dfdaadb257ce7a8980b3fdbd1a30539
NamSangKyu <nam2626@naver.com>
2026-06-05 11:48:46 +0900

    Add OrderController and update menu list template; implement order form functionality

--- step09-spring-thymleaf/src/main/java/com/spring/controller/OrderController.java
+++ step09-spring-thymleaf/src/main/java/com/spring/controller/OrderController.java
@@ -8,27 +8,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.spring.dto.MenuDTO;
+import com.spring.dto.OrderDTO;
 
 @Controller
-@RequestMapping("/menu")
-public class MenuController {
-
+@RequestMapping("/orders")
+public class OrderController {
 	private final List<MenuDTO> menuList = List.of(
 			new MenuDTO(1L, "불고기버거", "버거", 8500),
 			new MenuDTO(2L, "치킨버거", "버거", 9000), 
 			new MenuDTO(3L, "새우버거", "버거", 8000),
 			new MenuDTO(4L, "아이스아메리카노", "음료", 3500));
 	
-	@GetMapping("/list")
-	public ModelAndView list(ModelAndView view) {
+	@GetMapping("/new")
+	public ModelAndView orderForm(ModelAndView view, Long menuId) {
+		OrderDTO order = new OrderDTO();
+		if(menuId != null)
+			order.setMenuId(menuId); 
+		
+		view.addObject("orderForm", order);
 		view.addObject("menus", menuList);
-		view.setViewName("menu/list");
+		
 		return view;
 	}
-	
 }
-
-
 
 
 
