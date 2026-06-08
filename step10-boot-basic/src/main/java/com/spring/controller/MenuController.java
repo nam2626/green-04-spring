88790a014efed8c7b88dc25b2d0237e232f12e50
NamSangKyu <nam2626@naver.com>
2026-06-08 13:41:01 +0900

    Implement delete method in MenuController

--- step10-boot-basic/src/main/java/com/spring/controller/MenuController.java
+++ step10-boot-basic/src/main/java/com/spring/controller/MenuController.java
@@ -4,8 +4,10 @@ import com.spring.dto.MenuDTO;
 import com.spring.service.MenuService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -27,5 +29,13 @@ public class MenuController {
         view.setViewName("menu/list");
 
         return view;
+    }
+
+    @GetMapping("/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute(
+                "successMessage", "메뉴가 삭제되었습니다.");
+        menuService.delete(id);
+        return "redirect:/menus";
     }
 }
