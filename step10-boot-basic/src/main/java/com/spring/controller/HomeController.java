414a5d80fa5f9dd58b841c489e732f0ce9797bae
NamSangKyu <nam2626@naver.com>
2026-06-08 11:05:06 +0900

    Refactor HomeController to controller package and add MenuController with DTOs

--- step10-boot-basic/src/main/java/com/spring/controller/HomeController.java
+++ step10-boot-basic/src/main/java/com/spring/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.spring;
+package com.spring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
---
+++ step10-boot-basic/src/main/java/com/spring/dto/MenuDTO.java
@@ -0,0 +1,17 @@
+package com.spring.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+@AllArgsConstructor
+public class MenuDTO {
+    private long id;
+    private String name;
+    private int price;
+    private String category;
+    private String description;
+    private boolean available = true;
+}
