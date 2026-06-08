953236979f7d4fd7743208d9b3b6390f0a875e91
NamSangKyu <nam2626@naver.com>
2026-06-08 11:41:03 +0900

    Implement list method in MenuController to display all menus

--- step10-boot-basic/src/main/java/com/spring/controller/MenuController.java
+++ step10-boot-basic/src/main/java/com/spring/controller/MenuController.java
@@ -1,13 +1,31 @@
 package com.spring.controller;
 
+import com.spring.dto.MenuDTO;
+import com.spring.service.MenuService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/menus")
 public class MenuController {
 
-    
+    private MenuService menuService;
 
+    public MenuController(MenuService menuService) {
+        this.menuService = menuService;
+    }
 
+    @GetMapping
+    public ModelAndView list(ModelAndView view){
+        List<MenuDTO> list = menuService.findAll();
+
+        view.addObject("menus",list);
+        view.setViewName("menu/list");
+
+        return view;
+    }
 }
