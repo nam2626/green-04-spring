1021771635561eade811fc78563074d0c40064f8
NamSangKyu <nam2626@naver.com>
2026-06-09 17:17:00 +0900

    Add quest/step11 car-crud-project initial setup

---
+++ quest/step11/car-crud-project/src/main/java/com/spring/dto/CarDTO.java
@@ -0,0 +1,14 @@
+package com.spring.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.ibatis.type.Alias;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Alias("car")
+public class CarDTO {
+
+}
