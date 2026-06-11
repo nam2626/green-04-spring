d8825735329990f684509d154bf93e1d9c447188
NamSangKyu <nam2626@naver.com>
2026-06-11 14:02:44 +0900

    feat: Order 엔티티 추가

---
+++ step13-boot-jpa-join/src/main/java/com/spring/entity/Order.java
@@ -0,0 +1,59 @@
+package com.spring.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Table(name="order")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class Order {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NonNull
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id", nullable = false)
+	private Member member;
+	
+	@Column(name = "order_date", updatable = false)
+	private LocalDateTime orderDate;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false, length = 20)
+	private OrderStatus status = OrderStatus.PENDING;
+	
+	@PrePersist
+	public void prePersist() {
+		this.orderDate = LocalDateTime.now();
+	}
+
+}
+
+
+
+
+
+
+
+
