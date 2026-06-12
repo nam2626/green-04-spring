818c7946f12abdf31a4a88b396ea0274bb10e8bb
NamSangKyu <nam2626@naver.com>
2026-06-12 15:00:50 +0900

    feat: 주문 등록 폼 추가 및 주문 검색 파라미터명 수정 (OrderController, list.html)

--- step13-boot-jpa-join/src/main/java/com/spring/controller/OrderController.java
+++ step13-boot-jpa-join/src/main/java/com/spring/controller/OrderController.java
@@ -1,10 +1,13 @@
 package com.spring.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.spring.entity.Member;
+import com.spring.entity.Order;
 import com.spring.entity.OrderStatus;
 import com.spring.service.MemberService;
 import com.spring.service.MenuItemService;
@@ -24,15 +27,33 @@ public class OrderController {
 		this.orderService = orderService;
 	}
 
+//	@GetMapping
+//	public String list(Long memberId, OrderStatus status, Model model) {
+//		model.addAttribute("members", memberService.findAll());
+//		model.addAttribute("statuses", OrderStatus.values());
+//		model.addAttribute("orders", orderService.search(memberId,status));
+//		model.addAttribute("selectedMemberId", memberId);
+//		model.addAttribute("selectedStatus", status);
+//		
+//		return "order/list";
+//	}
 	@GetMapping
-	public String list(Long memberId, OrderStatus status, Model model) {
-		model.addAttribute("members", memberService.findAll());
-		model.addAttribute("statuses", OrderStatus.values());
-		model.addAttribute("orders", orderService.search(memberId,status));
-		model.addAttribute("selectedMemberId", memberId);
-		model.addAttribute("selectedStatus", status);
-		
-		return "order/list";
+	public ModelAndView orderList(ModelAndView view ,@RequestParam(required = false) Long memberId, @RequestParam(required = false) OrderStatus orderStatus ){
+		view.addObject("members", memberService.findAll());
+        view.addObject("statuses", OrderStatus.values());
+        view.addObject("orders" ,orderService.search(memberId,orderStatus));
+        view.addObject("selectedMemberId", memberId);
+        view.addObject("selectedStatus", orderStatus);
+        view.setViewName("order/list");
+        return view;
+	}
+	
+	@GetMapping("/new")
+	public ModelAndView form(ModelAndView view) {
+		view.addObject("members", memberService.findAll());
+		view.addObject("menuItems", menuItemService.findAll());
+		view.setViewName("order/form");
+		return view;
 	}
 	
 }
