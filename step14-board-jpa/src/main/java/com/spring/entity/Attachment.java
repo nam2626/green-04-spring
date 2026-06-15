65fc7a08c9e12972bee5835493d5918eb7761888
NamSangKyu <nam2626@naver.com>
2026-06-15 11:42:42 +0900

    feat: create Attachment entity for file management support

---
+++ step14-board-jpa/src/main/java/com/spring/entity/Attachment.java
@@ -0,0 +1,12 @@
+package com.spring.entity;
+
+/**
+ * 첨부파일 아이디(Long, PK)
+ * 첨부파일 원본 파일명(String)
+ * 첨부파일 저장한 파일명(String)
+ * 파일 사이즈(Long)
+ * 생성일(LocalDateTime)
+ */
+public class Attachment {
+
+}
