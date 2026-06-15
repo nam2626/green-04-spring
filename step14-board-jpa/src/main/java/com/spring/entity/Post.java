8244dd8793ceb1b8850f6d3757ee6ea1728cc715
NamSangKyu <nam2626@naver.com>
2026-06-15 11:07:30 +0900

    feat: create Post JPA entity with basic fields and lifecycle hooks

---
+++ step14-board-jpa/src/main/java/com/spring/entity/Post.java
@@ -0,0 +1,77 @@
+package com.spring.entity;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import net.bytebuddy.dynamic.TypeResolutionStrategy.Lazy;
+
+/**
+ * 게시글번호(Long, PK)
+ * 제목(String, NotNull)
+ * 내용(String, NotNull)
+ * 작성자(Member, name=member_id)
+ * 작성일(LocalDateTime, NotNull, default = CURRENT_TIMESTAMP)
+ * 수정일(LocalDateTime, default = CURRENT_TIMESTAMP)
+ * 조회수(Long, default = 0)
+ * 댓글목록(One-to-Many)
+ * 첨부파일 목록(One-to-Many)
+ */
+@Entity
+@Table(name = "post")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false, length = 200)
+  private String title;
+
+  @Column(nullable = false, columnDefinition = "TEXT")
+  private String content;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "member_id", nullable = false)
+  private Member member;
+
+  @Column(nullable = false)
+  private Long viewCount = 0L;
+
+  @Column(nullable = false)
+  private LocalDateTime createdAt;
+
+  @Column(nullable = false)
+  private LocalDateTime updatedAt;
+  // 댓글 목록
+  // private List<Comment> comments = new ArrayList<>();
+  // 첨부파일 목록
+  // private List<Attachement> attachements = new ArrayList<>();
+
+  @PrePersist
+  public void onCreate() {
+    createdAt = LocalDateTime.now();
+    updatedAt = LocalDateTime.now();
+  }
+
+  @PreUpdate
+  public void onUpdate() {
+    updatedAt = LocalDateTime.now();
+  }
+}
