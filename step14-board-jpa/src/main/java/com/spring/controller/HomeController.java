3cc5396f3efed0899a3e4d435af4523c78ed9fd5
NamSangKyu <nam2626@naver.com>
2026-06-15 14:17:50 +0900

    feat: add HomeController to redirect root path to /board

---
+++ step14-board-jpa/src/main/java/com/spring/controller/HomeController.java
@@ -0,0 +1,10 @@
+package com.spring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class HomeController {
+  @GetMapping("/")
+  public String home() {
+    return "redirect:/board";
+  }
+}
