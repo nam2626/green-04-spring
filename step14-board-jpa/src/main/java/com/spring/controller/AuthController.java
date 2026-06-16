891448053c3d86117e917bd08309494d638c28a6
NamSangKyu <nam2626@naver.com>
2026-06-16 11:50:51 +0900

    feat: add AuthController to handle user registration requests with validation

--- step14-board-jpa/src/main/java/com/spring/controller/AuthController.java
+++ step14-board-jpa/src/main/java/com/spring/controller/AuthController.java
@@ -1,7 +1,7 @@
 package com.spring.controller;
 
+import com.spring.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.bind.BindResult;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -14,18 +14,22 @@ import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 
 @Controller
 @RequestMapping("/auth")
 public class AuthController {
+  private final MemberRepository memberRepository;
   @Autowired
   private MemberService memberService;
+
+  AuthController(MemberRepository memberRepository) {
+    this.memberRepository = memberRepository;
+  }
 
   @GetMapping("/register")
   public String registerForm(Model model) {
@@ -35,10 +39,18 @@ public class AuthController {
   
   @PostMapping("/register")
   public String register(@Valid @ModelAttribute("form") MemberDTO form, 
-      BindingResult bindingResult, RequestAttributes requestAttributes) {
-      
-      return "redirect:/";
-      // return  "redirect:/auth/login";
+      BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+      System.out.println(form);
+      if(bindingResult.hasErrors()) return "auth/register";
+
+      try{
+        memberService.register(form);
+        redirectAttributes.addFlashAttribute("message", "회원가입이 완료되었습니다.");
+        return  "redirect:/auth/login";
+      }catch(IllegalArgumentException e){
+        bindingResult.reject(e.getMessage());
+        return "auth/register";
+      }
   }
   
 
