5ae0233011445121af4ceac8b11db3bd6ac75133
NamSangKyu <nam2626@naver.com>
2026-06-16 10:59:44 +0900

    feat: add AuthController to handle user registration routes and form binding

--- step14-board-jpa/src/main/java/com/spring/controller/AuthController.java
+++ step14-board-jpa/src/main/java/com/spring/controller/AuthController.java
@@ -1,13 +1,24 @@
 package com.spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.bind.BindResult;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.spring.dto.MemberDTO;
 import com.spring.service.MemberService;
+
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @Controller
@@ -18,8 +29,16 @@ public class AuthController {
 
   @GetMapping("/register")
   public String registerForm(Model model) {
-      model.addAttribute("member", new Member)
+      model.addAttribute("member", new MemberDTO());
       return "auth/register";
+  }
+  
+  @PostMapping("/register")
+  public String register(@Valid @ModelAttribute("form") MemberDTO form, 
+      BindingResult bindingResult, RequestAttributes requestAttributes) {
+      
+      return "redirect:/";
+      // return  "redirect:/auth/login";
   }
   
 
