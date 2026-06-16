b02d5880c8b7a20b5acafabe5b18dfb8e9d8395a
NamSangKyu <nam2626@naver.com>
2026-06-16 12:12:25 +0900

    feat: implement MemberService and MemberRepository for user registration with password encryption

--- step14-board-jpa/src/main/java/com/spring/repository/MemberRepository.java
+++ step14-board-jpa/src/main/java/com/spring/repository/MemberRepository.java
@@ -6,4 +6,9 @@ import com.spring.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+  boolean existsByUsername(String username);
+
+  boolean existsByNickname(String nickname);
+
+
 }
--- step14-board-jpa/src/main/java/com/spring/service/MemberService.java
+++ step14-board-jpa/src/main/java/com/spring/service/MemberService.java
@@ -1,8 +1,35 @@
 package com.spring.service;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import com.spring.dto.MemberDTO;
+import com.spring.entity.Member;
+import com.spring.repository.MemberRepository;
+
 
 @Service
 public class MemberService {
+
+  private final MemberRepository memberRepository;
+  private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+  public MemberService(MemberRepository memberRepository) {
+    this.memberRepository = memberRepository;
+  }
+
+  public void register(MemberDTO form) {
+    if(memberRepository.existsByUsername(form.getUsername()))
+      throw new IllegalArgumentException("중복된 아이디 입니다. 다른 아이디를 입력해 주세요");
+    if(memberRepository.existsByNickname(form.getNickname()))
+      throw new IllegalArgumentException("중복된 닉네임 입니다. 다른 닉네임을 입력해 주세요");
+
+    Member member = new Member();
+    member.setUsername(form.getUsername());
+    member.setNickname(form.getNickname());
+    member.setPassword(passwordEncoder.encode(form.getPassword()));
+    memberRepository.save(member);
+  }
 
 }
