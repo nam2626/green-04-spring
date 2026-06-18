8396e19ed57024564effea3b62a1e890ecba27da
NamSangKyu <nam2626@naver.com>
2026-06-18 17:05:44 +0900

    feat: implement ReactionController to handle post reaction logic and return updated counts

--- step14-board-jpa/src/main/java/com/spring/controller/ReactionController.java
+++ step14-board-jpa/src/main/java/com/spring/controller/ReactionController.java
@@ -3,6 +3,7 @@ package com.spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.spring.dto.ReactionDTO;
 import com.spring.entity.Member;
 import com.spring.entity.ReactionType;
 import com.spring.service.PostReactionService;
@@ -24,11 +25,18 @@ public class ReactionController {
   }
 
   @GetMapping("/post/{postId}/{type}")
-  public String postReaction(@PathVariable Long postId, @PathVariable String type, @SessionAttribute("loginMember") Member loginMember) {
+  public ReactionDTO postReaction(@PathVariable Long postId, @PathVariable String type, @SessionAttribute("loginMember") Member loginMember) {
       System.out.println(postId + " / " + type);
       ReactionType reactionType = ReactionType.valueOf(type.toUpperCase());
       postReactionService.addReaction(postId, reactionType, loginMember.getId());
-      return "안녕하세요";
+
+      ReactionDTO reactionDTO = new ReactionDTO();
+      //해당 게시글의 좋아요 값
+      reactionDTO.setLikes(postReactionService.getReactionCount(postId, ReactionType.LIKE));
+      //해당 게시글의 싫어요 값
+      reactionDTO.setDislikes(postReactionService.getReactionCount(postId, ReactionType.DISLIKE));
+
+      return reactionDTO;
   }
   
   
