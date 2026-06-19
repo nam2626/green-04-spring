355f3a595eb5c01c4edc13843775854cbf75f8d4
NamSangKyu <nam2626@naver.com>
2026-06-19 10:53:26 +0900

    feat: add CommentReactionService to handle comment reaction logic

--- step14-board-jpa/src/main/java/com/spring/service/CommentReactionService.java
+++ step14-board-jpa/src/main/java/com/spring/service/CommentReactionService.java
@@ -1,5 +1,6 @@
 package com.spring.service;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -11,8 +12,21 @@ import com.spring.dto.ReactionDTO;
 public class CommentReactionService {
 
   public Map<Long, ReactionDTO> getCommentReactions(List<Long> commentIds) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getCommentReactions'");
+    Map<Long, ReactionDTO> commentReactions = new LinkedHashMap<>();
+
+    Map<Long,Long> likes = new LinkedHashMap<>();
+    Map<Long,Long> dislikes = new LinkedHashMap<>();
+
+    // 반응이 없는 댓글고 화면에서는 0으로 출력할 수 있게 기본값을 셋팅
+    commentIds.forEach(item -> {
+      likes.put(item, 0L);
+      dislikes.put(item, 0L);
+    });
+
+    // 댓글 ID 목록을 기준으로 좋아요, 싫어요 개수를 한번에 집계
+    
+
+    return commentReactions;
   }
 
 }
