a5bfa4573d6c69718a7536f8d4cff11028ed1657
NamSangKyu <nam2626@naver.com>
2026-06-24 14:02:28 +0900

    Please provide the code changes or file diffs you would like me to summarize.

--- step17-board-backend/src/main/java/com/spring/controller/BoardController.java
+++ step17-board-backend/src/main/java/com/spring/controller/BoardController.java
@@ -1,9 +1,17 @@
 package com.spring.controller;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.spring.dto.BoardDTO;
 import com.spring.service.BoardService;
+import com.spring.vo.PaggingVO;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,4 +22,33 @@ public class BoardController {
 
   private final BoardService boardService;
   
+  // 게시글 조회
+  // 페이지 번호, 한 페이지당 게시글 개수, 검색어
+  @GetMapping
+  public ResponseEntity<Map<String,Object>> boardList(
+    @RequestParam(defaultValue = "") String keyword,
+    @RequestParam(defaultValue = "1") int page,
+    @RequestParam(defaultValue = "20") int size
+  ){
+    List<BoardDTO> boardList = null;
+    if(!keyword.isEmpty())
+      boardList = boardService.getBoardList(keyword,page,size);
+    else
+      boardList = boardService.searchBoardList(keyword,page,size);
+
+    int count = boardService.boardCount();
+    PaggingVO paggingVO = new PaggingVO(count, page);
+
+    Map<String,Object> map = new HashMap<String,Object>();
+    map.put("list",boardList);
+    map.put("pagging",paggingVO);
+
+    return ResponseEntity.ok(null);
+  }
+
+
 }
+
+
+
+
