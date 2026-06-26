61824f2f4a6748182010a797bfcf7bedad6dcabd
NamSangKyu <nam2626@naver.com>
2026-06-26 16:49:23 +0900

    feat: add AuthContext and AuthProvider for global authentication state management

--- step17-board-front/src/context/AuthContext.js
+++ step17-board-front/src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { authApi } from "../api/authApi";
 /**
  * 전역 인증 상태 관리
@@ -29,6 +29,41 @@ export function AuthProvider({children}){
   },[]);
 
   // 로그인
-
+  const login = async (username, password) => {
+    // 로그인 요청
+    const loginRes = await authApi.login({username,password});
+    // 토큰 저장
+    localStorage.setItem('accessToken',loginRes.data.accessToken);
+    localStorage.setItem('refreshToken',loginRes.data.refreshToken);
+    
+    // 사용자 정보 저장
+    const meRes = await authApi.me();
+    setUser(meRes.data);
+    return meRes.data;
+  };
   // 로그아웃
+  const logout = async () => {
+    try{
+      await authApi.logout();
+    }catch(error){
+      // 토큰이 만료된 상태
+    }
+    localStorage.clear();
+    setUser(null);
+  };
+
+  const isAuthenticated = !!user;
+
+  return (<AuthContext.Provider value={{
+    user,loading,isAuthenticated,login,logout
+  }}>{children}</AuthContext.Provider>)
+}
+
+/**
+ * 커스텀 훅
+ */
+export const useAuth = () => {
+  const ctx = useContext(AuthContext);
+  if(!ctx) throw new Error('useAuth는 AuthProvider 안에서만 사용 가능합니다.');
+  return ctx;
 }
