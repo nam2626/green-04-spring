00453a5ecd12ea931006ee6bb454beb51fe1dc65
NamSangKyu <nam2626@naver.com>
2026-06-26 11:00:49 +0900

    feat: create NavBar component with conditional authentication links

--- step17-board-front/src/components/NavBar.jsx
+++ step17-board-front/src/components/NavBar.jsx
@@ -1,7 +1,25 @@
-export default () => {
+import { useNavigate } from "react-router-dom"
 
+export default () => {
+  const isAuthenticate = false;
+  const navigate = useNavigate();
   return <nav>
-    <p>메뉴바</p>
+    <Link to="/">📄 Spring Board</Link>
+    <div>
+      {
+        isAuthenticate ?
+          <>
+            <span>사용자이름</span>
+            <Link to="/posts/create">글쓰기</Link>
+            <button type="button">로그아웃</button>
+          </>
+          :
+          <>
+            <Link to="/login">로그인</Link>
+            <Link to="/signup">회원가입</Link>
+          </>
+      }
+    </div>
 
   </nav>
 }
