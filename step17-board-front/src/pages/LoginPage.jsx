c046e0eed9c0dc8df16d0d02036030a7879b2cc5
NamSangKyu <nam2626@naver.com>
2026-06-26 17:15:51 +0900

    feat: create LoginPage component with authentication logic and navigation functionality

--- step17-board-front/src/pages/LoginPage.jsx
+++ step17-board-front/src/pages/LoginPage.jsx
@@ -1,7 +1,37 @@
+import { useState } from "react";
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+
 export default () => {
+  const username = useRef(null);
+  const password = useRef(null);
+  const [loading, setLoding] = useState(false);
+  const {login} = useAuth();
+  const navigate = useNavigate();
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleLogin = async () => {
+    setLoding(true);
+    try{
+      await login(username,password);
+      navigate('/');
+    }catch(error){
+      setErrorMessage(error.response?.data?.message || '로그인 실패했습니다.');
+    }finally{
+      setLoding(false);
+    }
+  };
+
   // 로그인 폼
   return <div className="container">
     <h2>로그인 페이지</h2>
+    <div className="frm-login">
+        <input type="text" placeholder="아이디를 입력하세요" ref={username}/>
+        <input type="password" placeholder="암호를 입력하세요" ref={password}/>
+        {errorMessage && <div className="error-message-box">{errorMessage}</div>}
+        {loading ? <p>현재 로그인 중입니다.</p> : <button onClick={handleLogin}>로그인</button>}
+        <button onClick={() => navigate('/auth/signup')}>회원가입</button>
 
+    </div>
   </div>
 }
