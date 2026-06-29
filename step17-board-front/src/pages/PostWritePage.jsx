6c70ce23a311d82c6fe9e13ae2be6f69c227db4e
NamSangKyu <nam2626@naver.com>
2026-06-29 12:18:05 +0900

    feat: 제목 및 내용 상태값 추가 및 적용

--- step17-board-front/src/pages/PostWritePage.jsx
+++ step17-board-front/src/pages/PostWritePage.jsx
@@ -1,28 +1,35 @@
 import { useContext, useState } from "react";
 import QuillEditor from "../components/QuillEditor"
+import { useParams } from "react-router-dom";
 
 
 export default () => {
   // 수정인지? 작성인지?
+  const {bno} = useParams();
+  const isEditorMode = !!bno;
+
   // 1. 제목, quill Editor 글내용 입력 받는 폼
+  const [form, setForm] = useState({title:'',content:''});
   // 2. 글쓰기 버튼 클릭시 게시글 전송
-  const defaultValue = "<h2>샘플 제목</h2><p>Quill <em>에디터</em> 테스트</p>";
-  const [postDetail, setPostDetail] = useState('');
 
   const onChangePostDetail = (newPostDetail) => {
-    setPostDetail(newPostDetail);
+    setForm({...form, content : newPostDetail});
+  }
+
+  const onChangeTitle = (e) => {
+    setForm({...form, title : e.taget.value});
   }
 
   return <div className="container">
-    <h2>글쓰기</h2>
+    <h2>{isEditorMode ? '게시글 수정' : '게시글 쓰기'}</h2>
     <div className="post-title">
       <label>제목</label>
-      <input type="text" placeholder="제목을 입력하세요"/>
+      <input type="text" placeholder="제목을 입력하세요" onChange={onChangeTitle} value={form.title}/>
     </div>
     <div className="post-detail">
       <label>내용</label>
       <div id="editor">
-        <QuillEditor onChange={onChangePostDetail} defaultVale={defaultValue}/>
+        <QuillEditor onChange={onChangePostDetail} defaultVale={form.content}/>
       </div>
     </div>
     <div className="error-box"></div>
